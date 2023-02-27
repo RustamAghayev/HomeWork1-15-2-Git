1d591467594225d4ad78331e75a0ad80e9d4aa31
Rustam <you@example.com>
2023-02-27 23:02:24 +0300

    Add Task

---
+++ HomeWork 1-15 (2)/HomeWork 1-15 (2)/Program.cs
@@ -0,0 +1,50 @@
+﻿#region task 1
+//int result = 0;
+//l1:
+//Console.WriteLine("4 reqemli ededi daxil edin:   ");
+//string numain = Console.ReadLine();
+//if (!int.TryParse(numain, out int num1))
+//{
+
+//    goto l1;
+
+//}
+//int numLong1 = (int)Math.Log10(num1) + 1;
+//if (numLong1 != 4)
+//{
+//    goto l1;
+//}
+//while (num1 > 0)
+//{
+//    result += num1 % 10;
+//    num1 = num1 / 10;
+//}
+//Console.WriteLine(result);
+#endregion
+#region task 2
+//int result = 0;
+//l1:
+//Console.WriteLine("6 reqemli ededi daxil edin:   ");
+//string numain = Console.ReadLine();
+//if (!int.TryParse(numain, out int num1))
+//{
+//    goto l1;
+//}
+//int NumberLong = (int)Math.Log10(num1) + 1;
+
+//int onenumber = num1 / 1000;
+
+//if (NumberLong != 6)
+//{
+//    goto l1;
+//}
+//while (onenumber > 0)
+//{
+//    result += onenumber % 10;
+//    onenumber = onenumber / 10;
+//}
+//Console.WriteLine(result);
+
+
+
+#endregion
